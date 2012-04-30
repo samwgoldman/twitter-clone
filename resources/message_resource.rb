656856f161cff7a219b15b5ad33c79030b0d0a9f
Sam Goldman <samwgoldman@gmail.com>
2012-04-30 02:00:01 -0400

    Use HamlResource as baseclass

--- resources/message_resource.rb
+++ resources/message_resource.rb
@@ -1,23 +1,10 @@
 require "message"
-require "haml"
 
-class MessageResource < Webmachine::Resource
-  def content_types_provided
-    [["application/xhtml+xml", :to_xhtml]]
-  end
-
+class MessageResource < HamlResource
   private
-
-  def template
-    File.read(File.expand_path("../../views/message.haml", __FILE__))
-  end
-
-  def view
-    Haml::Engine.new(template)
-  end
 
   def to_xhtml
     message = Message.find(request.path_info[:id])
-    view.render(message)
+    view("message").render(message)
   end
 end
--- resources/messages_resource.rb
+++ resources/messages_resource.rb
@@ -1,19 +1,14 @@
 require "message"
-require "haml"
 require "uri"
 require "id"
 
-class MessagesResource < Webmachine::Resource
+class MessagesResource < HamlResource
   def allowed_methods
     ["GET", "POST"]
   end
 
   def content_types_accepted
     [["application/x-www-form-urlencoded", :from_form]]
-  end
-
-  def content_types_provided
-    [["application/xhtml+xml", :to_xhtml]]
   end
 
   def post_is_create?
@@ -40,13 +35,5 @@ class MessagesResource < Webmachine::Resource
 
   def params
     Hash[URI.decode_www_form(request.body.to_s)]
-  end
-
-  def template
-    File.read(File.expand_path("../../views/messages.haml", __FILE__))
-  end
-
-  def view
-    Haml::Engine.new(template)
   end
 end
